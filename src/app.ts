c407da57cc2344dbd4b760a53e6d39d01f07c22c
unknown <dreicleal@hotmail.com>
2023-07-31 06:50:07 -0300

    server settings

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import "express-async-errors";
 import express, { Application } from "express";
+import bodyParser from "body-parser";
 
 export const app: Application = express();
+app.use(bodyParser.json())
+
+const PORT: number = 3000;
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
