a96d018db76d8e5ebe8cf56cf2ec9fd4b489efac
unknown <dreicleal@hotmail.com>
2023-08-10 15:13:40 -0300

    users interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 import { user, userSignUp } from "../schemas";
+import { DeepPartial } from "typeorm";
+import { User } from "../entities/users.entity";
 
 export type TUserCreated = z.infer<typeof user>
 
 export type TUserSignUp = z.infer<typeof userSignUp>
+
+export type TUserUpdate = DeepPartial<TUserSignUp>
+
+export type TUserRead = Array<User>
