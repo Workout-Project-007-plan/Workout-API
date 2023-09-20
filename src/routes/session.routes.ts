d69d3a277afa98dbc16a375d637ad61b6bcf3559
unknown <dreicleal@hotmail.com>
2023-09-20 14:03:12 -0300

    include Reactive account route

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createSessionController } from "../_controllers/session.controller";
+import { createSessionController, reactiveAccountController } from "../_controllers/session.controller";
 
 export const sessionRouter = Router()
 
 sessionRouter.post("", createSessionController)
+sessionRouter.patch("/reactive", reactiveAccountController)
